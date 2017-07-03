1507542063d00e31872ee60e8e3682f12b47d3bd
Josh Wood <wood2jp2@gmail.com>
2017-07-03 17:22:03 -0400

    firebase integrated, appending user input to table...

--- JS-CSS-Images/logic.js
+++ JS-CSS-Images/logic.js
@@ -4,8 +4,39 @@ var config = {
   authDomain: "trainschedule-ba8d8.firebaseapp.com",
   databaseURL: "https://trainschedule-ba8d8.firebaseio.com",
   projectId: "trainschedule-ba8d8",
-  storageBucket: "",
+  storageBucket: "trainschedule-ba8d8.appspot.com",
   messagingSenderId: "990670730957"
 };
-
 firebase.initializeApp(config);
+var database = firebase.database();
+
+$('.btn').on('click', function(event) {
+  event.preventDefault();
+  var name = $('#inputName').val().trim();
+  var destination = $('#inputDestination').val().trim();
+  var firstTrainTime = $('#inputTime').val().trim();
+  var frequency = $('#inputFrequency').val().trim();
+  console.log(name);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+  var newTrain = {
+    trainName: name,
+    trainDestination: destination,
+    arrivalTime: firstTrainTime,
+    trainFrequency: frequency,
+  }
+
+  database.ref().push(newTrain);
+
+  database.ref().on("child_added", function(childsnapshot) {
+    var newRow = $('<tr>');
+    var newData = $('<td>');
+
+    $('#trainNameData').append(childsnapshot.val().trainName + '<br>');
+    $('#destinationData').append(childsnapshot.val().trainDestination + '<br>');
+    $('#nextArrivalData').append(childsnapshot.val().arrivalTime + '<br>');
+    $('#frequencyData').append(childsnapshot.val().trainFrequency + '<br>');
+    // $('#tbody').append(newRow);
+  })
+});
