f1d3a3be361353e4b89cf82ec2431889ddf0453d
Josh Wood <wood2jp2@gmail.com>
2017-07-03 16:01:06 -0400

    HTML and CSS setup

---
+++ JS-CSS-Images/logic.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAYmnIgfQAgGkWg6uxNLp6YBiITMDDMjX0",
+  authDomain: "trainschedule-ba8d8.firebaseapp.com",
+  databaseURL: "https://trainschedule-ba8d8.firebaseio.com",
+  projectId: "trainschedule-ba8d8",
+  storageBucket: "",
+  messagingSenderId: "990670730957"
+};
+
+firebase.initializeApp(config);
